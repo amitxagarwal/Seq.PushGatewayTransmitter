6084e62210ec61bce4639fec8698d708d69400bf
DU00556591 <DU00556591@INBASDLP06496>
2019-07-30 15:54:37 +0530

    Add project files.

---
+++ Seq.PushGatewayTransmitter/PushgatewayReactor.cs
@@ -0,0 +1,62 @@
+﻿using Prometheus.Client;
+using Prometheus.Client.MetricPusher;
+using Seq.Apps;
+using Seq.Apps.LogEvents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushgatewayTransmitter
+{
+    [SeqApp("Seq.PushgatewayConnector",
+        Description = "Uses a Handlebars template to send events to Pushgateway.")]
+    public class PushgatewayReactor : Reactor, ISubscribeTo<LogEventData>
+    {
+        //private readonly object pushGatewayNotification;
+
+        [SeqAppSetting(
+            DisplayName = "Pushgateway URL",
+            HelpText = "The URL of the Pushgateway")]
+        public static string PushgatewayUrl { get; set; }
+
+        public void On(Event<LogEventData> evt)
+        {
+            var applicationName = FormatTemplate(evt);
+            var pushGatewayUrl = "https://kmd-shareddev-monitoring.westeurope.cloudapp.azure.com/pushgateway";
+            var seqPushgatewayWorkerName = "pushgateway-testworker";
+            var instanceName = "default";
+
+            var defaultPusher = new MetricPusher(pushGatewayUrl, seqPushgatewayWorkerName, instanceName);
+            IMetricPushServer server = new MetricPushServer(new IMetricPusher[]
+            {
+                defaultPusher
+            });
+            server.Start();
+            var counter = Metrics.CreateCounter("webjobcounter", "helptext", new[] { "ApplicationName" });
+            counter.Labels(applicationName).Inc();
+            server.Stop();
+        }
+
+        public static string FormatTemplate(Event<LogEventData> evt)
+        {
+            var properties = (IDictionary<string, object>)ToDynamic(evt.Data.Properties ?? new Dictionary<string, object>());
+            var resourceName = string.Empty;
+
+            foreach (var property in properties)
+            {
+                if (property.Key == "Application")
+                {
+                    resourceName = property.Value.ToString();
+                }
+            }
+            return resourceName;
+        }
+
+        private static IDictionary<string, object> ToDynamic(IReadOnlyDictionary<string, object> readOnlyDictionary)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
